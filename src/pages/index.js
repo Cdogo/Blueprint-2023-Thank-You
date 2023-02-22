9d2b553bef85e58f3c3271b503b234e36e52cb8c
Cdogo <john.tanaristy@gmail.com>
2023-02-22 10:28:58 -0600

    comments at the end

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 import ThankYouMessage from '@/components/ThankYouMessage'
+import ThankYouMessage1 from '@/components/Message'
 
 import { messages } from '@/components/messages'
 
@@ -30,11 +31,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className} flex flex-col justify-center items-center w-screen h-screen overflow-hidden`}>
-        <h1 id={styles.message}></h1>
-        <h2 id={styles.credit}>
-          Made with {'<'}3 by John Tan-Aristy
-        </h2>
+      <main className={`${inter.className} grid grid-rows-2 justify-center items-center w-screen h-screen overflow-hidden`}>
+        <div className = 'flex flex-col h-full justify-end'>
+          <h1 id={styles.message}></h1>
+          <h2 id={styles.credit}>
+            Made with {'<'}3 by John Tan-Aristy
+          </h2>
+        </div>
+        <div className = {`${styles.allmessages} w-fit flex justify-center items-center p-3 flex-wrap h-[50vh] max-w-screen-xl m-auto overflow-auto gap-3`}>
+          {messages && messages.map((msg) => {return(<ThankYouMessage1 {...msg}/>)})}
+        </div>
       </main>
       {messages && messages.map((msg) => {return(<ThankYouMessage {...msg}/>)})}
     </>
